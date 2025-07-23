f67d6979b11e97d7abd83b443cf8dd72e845447a
Nurzada <nurzadabakytkyzy8@gmail.com>
2025-07-23 08:12:35 -0700

    create class

--- src/main/java/org/example/DataStructuresAndAlgorithms.java
+++ src/main/java/org/example/DataStructuresAndAlgorithms.java
@@ -2,33 +2,33 @@ package org.example;
 
 import java.util.Scanner;
 
-public class DataStructuresAndAlgorithms {
+public class DataStructuresAndAlgorithms{
 
-        public void LinearSearch() {
-            Scanner scanner = new Scanner(System.in);
+    public void LinearSearch() {
+        Scanner scanner = new Scanner(System.in);
 
-            // Read the size of the array
-            int N = scanner.nextInt();
+        // Read the size of the array
+        int N = scanner.nextInt();
 
-            // Read the array elements
-            int[] arr = new int[N];
-            for (int i = 0; i < N; i++) {
-                arr[i] = scanner.nextInt();
-            }
-
-            // Read the number to search for
-            int x = scanner.nextInt();
-
-            // Count occurrences of x in the array
-            int count = 0;
-            for (int num : arr) {
-                if (num == x) {
-                    count++;
-                }
-            }
-
-            // Output the result
-            System.out.println(count);
+        // Read the array elements
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
         }
+
+        // Read the number to search for
+        int x = scanner.nextInt();
+
+        // Count occurrences of x in the array
+        int count = 0;
+        for (int num : arr) {
+            if (num == x) {
+                count++;
+            }
+        }
+
+        // Output the result
+        System.out.println(count);
+    }
 
 }
